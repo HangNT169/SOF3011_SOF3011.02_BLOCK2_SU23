0fab6a7166b12217d122fd2c57cdfd44121f87c5
HangNT169 <HangNT169@fpt.edu.vn>
2023-07-01 16:14:26 +0700

    B3: CRUD List Fix Cung P1

--- src/main/java/com/poly/soft3011/hangnt169/B3_CRUDListFixCung/service/SinhVienServiceImpl.java
+++ src/main/java/com/poly/soft3011/hangnt169/B3_CRUDListFixCung/service/SinhVienServiceImpl.java
@@ -15,7 +15,11 @@ public class SinhVienServiceImpl implements SinhVienService {
 
     public SinhVienServiceImpl() {
         // add 5 phan tu vao day
-
+        listSinhVien.add(new SinhVien("SV1","tuana",20,"abc",true));
+        listSinhVien.add(new SinhVien("SV2","tuanb",21,"abc",false));
+        listSinhVien.add(new SinhVien("SV3","tuanc",22,"abc",true));
+        listSinhVien.add(new SinhVien("SV4","tuand",23,"abc",false));
+        listSinhVien.add(new SinhVien("SV5","tuane",24,"abc",true));
     }
 
     @Override
